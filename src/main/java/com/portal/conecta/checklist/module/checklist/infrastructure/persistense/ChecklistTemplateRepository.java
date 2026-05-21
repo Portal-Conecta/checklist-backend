3cf2848c3655dfcee6724b9bb5a82810bbe1111e
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-05-20 21:51:39 -0300

    Feat:implements ChecklistTemplaterepository file

--- src/main/java/com/portal/conecta/checklist/module/checklist/infrastructure/persistense/ChecklistTemplateRepository.java
+++ src/main/java/com/portal/conecta/checklist/module/checklist/infrastructure/persistense/ChecklistTemplateRepository.java
@@ -1,14 +1,19 @@
 package com.portal.conecta.checklist.module.checklist.infrastructure.persistense;
 
+import com.portal.conecta.checklist.module.checklist.domain.model.ChecklistExecution;
 import com.portal.conecta.checklist.module.checklist.domain.model.ChecklistTemplate;
-import org.hibernate.validator.constraints.UUID;
+
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
-public interface ChecklistTemplateRepository extends JpaRepository<UUID, ChecklistTemplate> {
+public interface ChecklistTemplateRepository extends JpaRepository<ChecklistTemplate,UUID > {
 
 
 
@@ -21,11 +26,34 @@ public interface ChecklistTemplateRepository extends JpaRepository<UUID, Checkli
             (@Param("roomId") UUID roomId);
 
     @Query(value = """
-                SELECT()
-            
+                SELECT id
+                FROM checklist_template
+                WHERE room_id = :roomId
+                AND active = true 
+                AND status = 'SUBMITTED'
+                ORDER BY version Desc
+                LIMIT 1
+            """, nativeQuery = true)
+    Optional<UUID> findLastestTemplateByRoomId(@Param("roomId")UUID roomId);
+
+    @Query(value = """
+            SELECT COUNT(id)
+            FROM checklist_template
+            WHERE room_id = :roomId
+            AND active = true
+            AND status = 'SUBMITTED'
+            GROUP BY room_id
+            HAVING COUNT(id) > 1
             
             """, nativeQuery = true)
-    ;
+    Long countActivAndSubmittedTemplatesByIdRoomId(@Param("roomid")UUID roomId);
+
+    @Query(value = """
+        SELECT *
+        FROM checklist_template
+        WHERE id = :templateId
+        """, nativeQuery = true)
+    Optional<ChecklistTemplate> findTemplateByIdNative(@Param("templateId") UUID templateId);
 
 
 

a30695192935dab6eeefac54bc99da43cefd04e5
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2026-05-22 19:41:28 -0300

    fix: align local template schema constraints

--- src/main/java/com/portal/conecta/checklist/module/checklist/domain/model/ChecklistTemplate.java
+++ src/main/java/com/portal/conecta/checklist/module/checklist/domain/model/ChecklistTemplate.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.time.Instant;

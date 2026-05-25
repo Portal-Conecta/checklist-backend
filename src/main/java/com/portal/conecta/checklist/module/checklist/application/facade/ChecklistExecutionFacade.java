560e637c10951b7fdcf5ec5af991775e655db82c
Daniel Rios <daniel_sismer@estudante.sesisenai.org.br>
2026-05-24 21:05:27 -0300

    feat: expoe endpoint para enviar execucao de checklist

--- src/main/java/com/portal/conecta/checklist/module/checklist/application/facade/ChecklistExecutionFacade.java
+++ src/main/java/com/portal/conecta/checklist/module/checklist/application/facade/ChecklistExecutionFacade.java
@@ -2,18 +2,23 @@ package com.portal.conecta.checklist.module.checklist.application.facade;
 
 
 import com.portal.conecta.checklist.module.checklist.application.usecase.execution.CreateChecklistExecutionUseCase;
+import com.portal.conecta.checklist.module.checklist.application.usecase.execution.SubmitChecklistExecutionUseCase;
 import com.portal.conecta.checklist.module.checklist.domain.model.ChecklistExecution;
 import com.portal.conecta.checklist.module.checklist.presentation.dto.request.ChecklistExecutionDraftCreateDTO;
+import com.portal.conecta.checklist.module.checklist.presentation.dto.request.ChecklistExecutionSubmitDTO;
 import com.portal.conecta.checklist.module.checklist.presentation.dto.response.ChecklistExecutionResponseDTO;
 import com.portal.conecta.checklist.module.checklist.presentation.mapper.ChecklistExecutionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class ChecklistExecutionFacade {
 
 private final CreateChecklistExecutionUseCase createChecklistExecutionUseCase;
+private final SubmitChecklistExecutionUseCase submitChecklistExecutionUseCase;
 private final ChecklistExecutionMapper executionMapper;
 
     public ChecklistExecutionResponseDTO createDTO(ChecklistExecutionDraftCreateDTO request){
@@ -22,4 +27,8 @@ private final ChecklistExecutionMapper executionMapper;
         return  executionMapper.toResponse(execution);
     }
 
+    public ChecklistExecutionResponseDTO submit(UUID executionId, ChecklistExecutionSubmitDTO request) {
+        ChecklistExecution execution = submitChecklistExecutionUseCase.execute(executionId, request);
+        return executionMapper.toResponse(execution);
+    }
 }
--- src/main/java/com/portal/conecta/checklist/module/checklist/presentation/controller/ChecklistExecutionController.java
+++ src/main/java/com/portal/conecta/checklist/module/checklist/presentation/controller/ChecklistExecutionController.java
@@ -1,19 +1,20 @@
 package com.portal.conecta.checklist.module.checklist.presentation.controller;
 
 import com.portal.conecta.checklist.module.checklist.application.facade.ChecklistExecutionFacade;
-import com.portal.conecta.checklist.module.checklist.application.usecase.execution.CreateChecklistExecutionUseCase;
 import com.portal.conecta.checklist.module.checklist.presentation.dto.request.ChecklistExecutionDraftCreateDTO;
+import com.portal.conecta.checklist.module.checklist.presentation.dto.request.ChecklistExecutionSubmitDTO;
 import com.portal.conecta.checklist.module.checklist.presentation.dto.response.ChecklistExecutionResponseDTO;
-import com.portal.conecta.checklist.module.checklist.presentation.mapper.ChecklistExecutionMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/checklist-executions")
@@ -26,5 +27,13 @@ public class ChecklistExecutionController {
     @PostMapping("/drafts")
     public ResponseEntity<ChecklistExecutionResponseDTO> createDraft(@RequestBody @Valid ChecklistExecutionDraftCreateDTO request) {
       return ResponseEntity.status(HttpStatus.CREATED).body(checklistExecutionFacade.createDTO(request));
+    }
+
+    @PostMapping("/{executionId}/submit")
+    public ResponseEntity<ChecklistExecutionResponseDTO> submit(
+            @PathVariable UUID executionId,
+            @RequestBody @Valid ChecklistExecutionSubmitDTO request
+    ) {
+        return ResponseEntity.ok(checklistExecutionFacade.submit(executionId, request));
     }
 }

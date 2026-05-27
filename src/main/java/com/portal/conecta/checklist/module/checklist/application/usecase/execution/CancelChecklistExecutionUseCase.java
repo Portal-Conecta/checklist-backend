64d174f45f0c30a4034587889737fe1727a99940
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-05-27 14:47:27 -0300

    feat: cria o use-case para o cancel do execution

---
+++ src/main/java/com/portal/conecta/checklist/module/checklist/application/usecase/execution/CancelChecklistExecutionUseCase.java
@@ -0,0 +1,37 @@
+package com.portal.conecta.checklist.module.checklist.application.usecase.execution;
+
+import com.portal.conecta.checklist.module.checklist.domain.enums.ChecklistExecutionStatus;
+import com.portal.conecta.checklist.module.checklist.domain.model.ChecklistExecution;
+import com.portal.conecta.checklist.module.checklist.infrastructure.persistence.ChecklistExecutionRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CancelChecklistExecutionUseCase {
+
+
+    private final ChecklistExecutionRepository executionRepository;
+
+
+    @Transactional
+    public ChecklistExecution execute(UUID executionId){
+        ChecklistExecution execution = executionRepository.findById(executionId)
+                .orElseThrow(() -> new EntityNotFoundException("Execucao de checklist nao encontrada"));
+
+        if(execution.getStatus() != ChecklistExecutionStatus.SUBMITTED){
+            throw new IllegalArgumentException("Somente checklist enviados podem ser cancelados");
+        }
+        execution.setStatus(ChecklistExecutionStatus.CANCELED);
+
+        return executionRepository.save(execution);
+
+
+    }
+
+
+}

a1280344058f08f6804cc3fed91fc50122381927
MATHEUS ALEXANDRE DE CASTRO <matheus_a_castro@estudante.sesisenai.org.br>
2026-05-29 20:21:08 -0300

    #102 docs: Adicionando JavaDoc nas classes de exceções

--- src/main/java/com/portal/conecta/checklist/shared/exception/ErrorResponseDTO.java
+++ src/main/java/com/portal/conecta/checklist/shared/exception/ErrorResponseDTO.java
@@ -3,6 +3,25 @@ package com.portal.conecta.checklist.shared.exception;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+/**<h1>DTO para erros </h1>
+ * <p>
+ * Essa classe vai certralizar informações criticas de uma falha que facilita
+ * o tratamento de erros de que consumir endpoints
+ * </p>
+ *  <p><b>Exemplo de estrutura JSON gerada:</b></p>
+ *  <pre>
+ *      {
+ *          "locaDateTime": "2026-0529T19:40:00",
+ *          "status": 400,
+ *          "message":  "Erro de validação",
+ *          "errors": {"email": "E-mail inválido"}
+ *      }
+ *  </pre>
+ * @param localDateTime Tempo exato de quando ocorreu o erro.
+ * @param status Código de status HTTP da requisição (ex: 400, 404, 500).
+ * @param message Uma mensagem geral para descrever erros.
+ * @param errors Um mapa que contem detalhes especificos dos erros.
+ */
 public record ErrorResponseDTO(
         LocalDateTime localDateTime,
         int status,

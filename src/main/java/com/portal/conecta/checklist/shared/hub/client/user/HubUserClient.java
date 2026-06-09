71cf9b8b21e2be216863fbbf65ea4a291f5df42d
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2026-06-09 19:02:37 -0300

    #150 feat: tipa contrato de vinculos academicos do Hub

--- src/main/java/com/portal/conecta/checklist/shared/hub/client/user/HubUserClient.java
+++ src/main/java/com/portal/conecta/checklist/shared/hub/client/user/HubUserClient.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface HubUserClient {
 
     @GetMapping("/me/courses")
-    void validateCurrentUser();
+    HubMyListCourseResponse findMyCourses();
 }

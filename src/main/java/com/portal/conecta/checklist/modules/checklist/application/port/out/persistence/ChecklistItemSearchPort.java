b7d8846f76c97e2c50bd87b23bd08155146c22fe
MATHEUS ALEXANDRE DE CASTRO <matheus_a_castro@estudante.sesisenai.org.br>
2026-06-19 15:14:19 -0300

    #147 feat: adiciona porta ChecklistItemSearchPort para consulta de itens

---
+++ src/main/java/com/portal/conecta/checklist/modules/checklist/application/port/out/persistence/ChecklistItemSearchPort.java
@@ -0,0 +1,9 @@
+package com.portal.conecta.checklist.modules.checklist.application.port.out.persistence;
+
+import com.portal.conecta.checklist.modules.checklist.domain.schema.ChecklistItem;
+
+import java.util.List;
+
+public interface ChecklistItemSearchPort {
+    List<ChecklistItem> searchByTitleOrDescription(String searchTerm);
+}

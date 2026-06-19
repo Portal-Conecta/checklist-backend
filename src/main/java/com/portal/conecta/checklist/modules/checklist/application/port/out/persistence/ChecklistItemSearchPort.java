a3d6218c1a5f34f4719c3761b16d4afc89cb8da7
MATHEUS ALEXANDRE DE CASTRO <matheus_a_castro@estudante.sesisenai.org.br>
2026-06-19 16:58:39 -0300

    #147 feat: atualiza definicao de metodos na ChecklistItemSearchPort

--- src/main/java/com/portal/conecta/checklist/modules/checklist/application/port/out/persistence/ChecklistItemSearchPort.java
+++ src/main/java/com/portal/conecta/checklist/modules/checklist/application/port/out/persistence/ChecklistItemSearchPort.java
@@ -1,7 +1,6 @@
 package com.portal.conecta.checklist.modules.checklist.application.port.out.persistence;
 
 import com.portal.conecta.checklist.modules.checklist.domain.schema.ChecklistItem;
-
 import java.util.List;
 
 public interface ChecklistItemSearchPort {

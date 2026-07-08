7518a8a9b9e97857f76c770a61387664ef222986
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-07-08 14:01:41 -0300

    #182 fix:corrigi classe notificationEventType

--- src/main/java/com/portal/conecta/checklist/shared/messaging/notification/NotificationEventType.java
+++ src/main/java/com/portal/conecta/checklist/shared/messaging/notification/NotificationEventType.java
@@ -2,9 +2,11 @@ package main.java.com.portal.conecta.checklist.shared.messaging.notification;
 
 public final class NotificationEventType {
 
-    private NotificationEventType() {} 
+    private NotificationEventType() {}
 
     public static final String CHECKLIST_MISSED_DEADLINE = "checklist.missed_deadline";
     public static final String CHECKLIST_THREE_DAYS_MISSING = "checklist.three_days_missing";
     public static final String CHECKLIST_SUBMITTED = "checklist.submitted";
+
+    public static final String CHECKLIST_NON_COMPLIANCE_CREATED = "checklist.non_compliance.created";
 }

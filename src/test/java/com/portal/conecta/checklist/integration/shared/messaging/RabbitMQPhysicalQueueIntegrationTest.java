ff4b75ad988d614d485129a2aa4d367d99e93234
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2026-07-10 18:26:32 -0300

    fix: troca webEnviroment de NONE para MOCK e add novo paremetro

--- src/test/java/com/portal/conecta/checklist/integration/shared/messaging/RabbitMQPhysicalQueueIntegrationTest.java
+++ src/test/java/com/portal/conecta/checklist/integration/shared/messaging/RabbitMQPhysicalQueueIntegrationTest.java
@@ -12,9 +12,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -26,7 +25,6 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers(disabledWithoutDocker = true)
-
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         properties = {
@@ -47,15 +45,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RabbitMQPhysicalQueueIntegrationTest {
 
     @Container
+    @ServiceConnection
     static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3.13-management");
-
-    @DynamicPropertySource
-    static void overrideRabbitConnectionProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
-        registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
-        registry.add("spring.rabbitmq.username", rabbitMQContainer::getAdminUsername);
-        registry.add("spring.rabbitmq.password", rabbitMQContainer::getAdminPassword);
-    }
 
     @Autowired
     private NotificationEventPublisher publisher;

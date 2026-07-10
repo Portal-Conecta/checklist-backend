cd569ccdd9d7b98ef6ccaf20d1c48053a1cef9f9
DANIEL VINICIUS RIOS SISMER <daniel_sismer@estudante.sesisenai.org.br>
2026-07-10 18:39:19 -0300

    fix: remove SuiteTest redundante que causava execucao dupla dos testes
    O SuiteTest (@Suite + @SelectPackages) reexecutava todo o pacote alem da
    descoberta automatica do Surefire, rodando cada classe 2x. Isso quebrava o
    RabbitMQPhysicalQueueIntegrationTest (Testcontainers/@ServiceConnection) na
    segunda execucao com "Connection refused", falhando o CI.

--- src/test/java/com/portal/conecta/checklist/unit/shared/SuiteTest.java
+++
@@ -1,9 +0,0 @@
-package com.portal.conecta.checklist.unit.shared;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages("com.portal.conecta.checklist")
-public class SuiteTest {
-}
